d35a9a83a7a4be5ab11d52cf5e1e1ef7ed07c942
md-arif-shaikh <arifshaikh.astro@gmail.com>
2023-07-01 16:05:34 +0900

    fix delta_f resolution

--- bilby_source/nr/source.py
+++ bilby_source/nr/source.py
@@ -7,13 +7,14 @@ import numpy as np
 
 
 def frequency_domain_nr_source_model(
-    frequecny,
+    frequency,
     mass_1,
     mass_2,
     luminosity_distance,
     theta_jn,
     phase,
-    data_file
+    data_file,
+    **kwargs
 ):
     """Frequency domain source model for injecting NR data.
 
@@ -37,20 +38,66 @@ def frequency_domain_nr_source_model(
     t = d["t"][:] * lal.MTSUN_SI * m_total
     t = t - t[0]
     h22 = d["h22"][:]
-    dt = 1/(2 * max(frequecny))
+    dt = 1/(2 * max(frequency))
     time = np.arange(t[0], t[-1], dt)
-    h22_interp = (np.interp(time, t, h22)
+    amp22 = np.abs(h22)
+    phase22 = - np.unwrap(np.angle(h22))
+    amp22_interp = np.interp(time, t, amp22)
+    phase22_interp = np.interp(time, t, phase22)
+    h22_interp = (amp22_interp
                   * (1/(luminosity_distance * 10**6 * lal.PC_SI))
-                  * (lal.C_SI * m_total * lal.MTSUN_SI))
+                  * (lal.C_SI * m_total * lal.MTSUN_SI)
+                  * np.exp(-1j * phase22_interp))
     h = lal.SpinWeightedSphericalHarmonic(
         theta_jn, phase, -2, 2, 2) * h22_interp
     hp = taper_timeseries(TimeSeries(np.real(h), delta_t=dt),
                           tapermethod='startend')
     hc = taper_timeseries(TimeSeries(- np.imag(h), delta_t=dt),
                           tapermethod='startend')
-    hpf = hp.to_frequencyseries()
-    hcf = hc.to_frequencyseries()
-    plus = np.interp(frequecny, hpf.sample_frequencies, hpf.data)
-    cross = np.interp(frequecny, hcf.sample_frequencies, hcf.data)
+    df = frequency[1] - frequency[0]
+    hpf = hp.to_frequencyseries(delta_f=df)
+    hcf = hc.to_frequencyseries(delta_f=df)
+    plus = np.interp(frequency, hpf.sample_frequencies, hpf.data)
+    cross = np.interp(frequency, hcf.sample_frequencies, hcf.data)
     return {"plus": plus,
             "cross": cross}
+
+
+def time_domain_nr_source_model(
+        time,
+        mass_1,
+        mass_2,
+        luminosity_distance,
+        theta_jn,
+        phase,
+        data_file,
+        **kwargs):
+    """Time domain source model for injecting NR data.
+
+    Parameters:
+    -----------
+    mass_1: float
+        First component mass in units of solar mass to scale the NR waveform.
+    mass_2: float
+        Second component mass in units of solar mass to scale the NR waveform.
+    luminosity_distance: float
+        Luminosity distance in units of Mpc to scale the NR waveform.
+    theta_jn: float
+        Inclination angle to compute sYlm.
+    phase: float
+        phase to compute the sYlm.
+    data_file: str
+        Path to the NR data file.
+    """
+    d = h5py.File(data_file, "r")
+    m_total = mass_1 + mass_2
+    t = d["t"][:] * lal.MTSUN_SI * m_total
+    t = t - t[0]
+    h22 = d["h22"][:]
+    h22_interp = (np.interp(time, t, h22)
+                  * (1/(luminosity_distance * 10**6 * lal.PC_SI))
+                  * (lal.C_SI * m_total * lal.MTSUN_SI))
+    h = lal.SpinWeightedSphericalHarmonic(
+        theta_jn, phase, -2, 2, 2) * h22_interp
+    return {"plus": h.real,
+            "cross": -h.imag}
